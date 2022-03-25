1d6c7407089a038de654f961d108259f120246ba
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2022-03-25 11:20:11 -0300

    test: size with 1 item

--- src/mystack.test.js
+++ src/mystack.test.js
@@ -4,3 +4,9 @@ test("empty stack must have size() === 0", () => {
     const stack = new MyStack();
     expect(stack.size()).toBe(0);
 })
+
+test("stack with 1 item must have size() === 1", () => {
+    const stack = new MyStack();
+    stack.push('A');
+    expect(stack.size()).toBe(1);
+})
